1b11e22dd8738829e0ad6689846aefd229f9896a
chlqhgus <bocurent@gmail.com>
2024-12-14 20:01:56 +0900

    re-initialized

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles/App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+
+const App = () => {
+  return <MainPage />;
+};
+
+export default App;
